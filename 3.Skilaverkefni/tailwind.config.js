a252fe0408165141c078f7acfaccce6281acdb42
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-02-27 07:56:23 +0000

    skilaverkefni 3

---
+++ 3.Skilaverkefni/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        '2/5': '40%',
+      },
+    },
+  },
+  plugins: [],
+}
+
