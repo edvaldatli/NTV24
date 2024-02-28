dcf55f5a1cddff7a3fb0bacbb6d5aedf40e77c2d
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-02-28 20:42:36 +0000

    skilaverkefni 4.2 lokið

--- 4.Skilaverkefni/prt2/tailwind.config.js
+++ 4.Skilaverkefni/prt2/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    boxShadow: {
+      "blue-md": '0 2px 3px -1px rgba(59 130 246 / 40%)'
+    },
     extend: {},
   },
   plugins: [],
