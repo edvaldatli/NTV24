b08ef1690ac02be25c1501813bcc2263425d4470
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-03-10 15:09:11 +0000

    menu update

--- 1.Assignment/src/scripts/script.js
+++ 1.Assignment/src/scripts/script.js
@@ -1,23 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('toggleDrawer');
-    const drawer = document.getElementById('drawer');
+function scrollToSection(buttonId, sectionId) {
+    $(buttonId).on('click', () => {
+        $('html, body').animate({ scrollTop: $(sectionId).offset().top -100}, 500);
+    });
+};
 
-    toggleButton.addEventListener('click', () => {
-        if (drawer.classList.contains('translate-y-0')) {
-            drawer.classList.replace('translate-y-0', '-translate-y-full');
-            setTimeout(() => drawer.classList.add('hidden'), 500);
+$(document).ready(() => {
+    const toggleButton = $('#toggleDrawer');
+    const drawer = $('#drawer');
+
+    // Open and close drawer
+    toggleButton.on('click', () => {
+        if (drawer.hasClass('translate-y-0')) {
+            drawer.removeClass('translate-y-0').addClass('-translate-y-full');
+            setTimeout(() => drawer.addClass('hidden'), 500);
         } else {
-            drawer.classList.remove('hidden');
-            setTimeout(() => drawer.classList.replace('-translate-y-full', 'translate-y-0'), 10);
+            drawer.removeClass('hidden');
+            setTimeout(() => drawer.removeClass('-translate-y-full').addClass('translate-y-0'), 10);
         }
     });
-    
-    document.addEventListener('click', (e) => {
-        console.log(e.target);
-        if (drawer.classList.contains('translate-y-0') && !drawer.contains(e.target) && e.target !== toggleButton) {
-            drawer.classList.replace('translate-y-0', '-translate-y-full');
-            setTimeout(() => drawer.classList.add('hidden'), 500);
+
+    // Close drawer when clicking outside of it
+    $(document).on('click', (e) => {
+        if (drawer.hasClass('translate-y-0') && !drawer.has(e.target).length && e.target !== toggleButton[0]) {
+            drawer.removeClass('translate-y-0').addClass('-translate-y-full');
+            setTimeout(() => drawer.addClass('hidden'), 500);
         }
     });
+
+    // Back to top button
+    $('#backToTop').on('click', () => {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    });
+
+    // Show back to top button when scrolling down
+    $(window).on('scroll', () => {
+        if ($(window).scrollTop() > 100) {
+            $('#backToTop').removeClass('hidden');
+        } else {
+            $('#backToTop').addClass('hidden');
+        }
+    });
+
+
+
+    scrollToSection('#starters', '#starters-section');
+    scrollToSection('#courses', '#courses-section');
+    scrollToSection('#sandwiches', '#sandwiches-section');
+    scrollToSection('#sides', '#sides-section');
 });
+
+
   
